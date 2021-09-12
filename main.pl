7c675a4470bdfbdfecd5fef099fc6d406f84ce3a
Thiago do Prado <thiago@bgcbrasil.com.br>
2021-09-12 18:21:22 -0300

    feat: create functions to know cell's state

--- main.pl
+++ main.pl
@@ -3,3 +3,33 @@ estado_inicial([
     [0,3,2],
     [0,0,4]
 ]).
+
+limpo(X,Y):-
+    estado_inicial(A),
+    nth0(Y, A, Linha),
+    nth0(X, Linha, Elem),
+    Elem = 0.
+
+aspirador(X,Y):-
+    estado_inicial(A),
+    nth0(Y, A, Linha),
+    nth0(X, Linha, Elem),
+    Elem = 1.
+
+sujo(X,Y):-
+    estado_inicial(A),
+    nth0(Y, A, Linha),
+    nth0(X, Linha, Elem),
+    Elem = 2.
+
+barreira(X,Y):-
+    estado_inicial(A),
+    nth0(Y, A, Linha),
+    nth0(X, Linha, Elem),
+    Elem = 3.
+
+base(X,Y):-
+    estado_inicial(A),
+    nth0(Y, A, Linha),
+    nth0(X, Linha, Elem),
+    Elem = 4.
